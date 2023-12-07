c75e76459cce40621a735717b81e0b3c9871f050
yuvik777 <94516555+yuvik777@users.noreply.github.com>
2023-12-07 13:13:22 +0000

    update config

--- Deployment/frontend/conf/entrypoint.sh
+++ Deployment/frontend/conf/entrypoint.sh
@@ -3,6 +3,6 @@
 set -eu
 
 echo "update Nginx config"
-envsubst '\${HOST1} \${HOST2} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
+envsubst '\${HOST1} \${HOST2} \${HOST3} \${PORT}' < /usr/share/nginx/html/config/default.conf > /etc/nginx/nginx.conf
 
 exec "$@"
